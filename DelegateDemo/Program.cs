fcf4776b4b4f0fc8b65e4d6241bd44c3c711b6ab
ADMIN <sahilbambhania@gmail.com>
2022-08-18 17:12:16 +0530

    Add files

---
+++ DelegateDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DelegateDemo;
+using static DelegateDemo.ExampleDemo;
+
+Console.WriteLine("Delegate Demo");
+//operation obj = new operation(Addition);
+//Console.WriteLine("Addition is = {0}", obj(23, 27));
+MulticastDelegate1.ImplementDelegate();
+Console.ReadKey();
+
